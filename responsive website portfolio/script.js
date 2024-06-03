55d1e80e1f3c56f074af406e76b76fddad530ddd
MANOJ PALOI <96537673+ManojPaloi@users.noreply.github.com>
2024-06-04 01:56:45 +0530

    Add files via upload

---
+++ responsive website portfolio/script.js
@@ -0,0 +1,68 @@
+let words = document.querySelectorAll(".word");
+words.forEach((word) => {
+  let letters = word.textContent.split("");
+  word.textContent = "";
+  letters.forEach((letter) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.className = "letter";
+    word.append(span);
+  });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+let changeText = () => {
+  let currentWord = words[currentWordIndex];
+  let nextWord =
+    currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+  Array.from(currentWord.children).forEach((letter, i) => {
+    setTimeout(() => {
+      letter.className = "letter out";
+    }, i * 80);
+  });
+  nextWord.style.opacity = "1";
+  Array.from(nextWord.children).forEach((letter, i) => {
+    letter.className = "letter behind";
+    setTimeout(() => {
+      letter.className = "letter in";
+    }, 340 + i * 80);
+  });
+  currentWordIndex =
+    currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+changeText();
+setInterval(changeText, 3000);
+
+//////------skill circles--------///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const circles = document.querySelectorAll(".circle");
+circles.forEach((elem) => {
+  var dots = elem.getAttribute("data-dots");
+  var marked = elem.getAttribute("data-percent");
+  var percent = Math.floor((dots * marked) / 100);
+  var points = "";
+  var rotate = 360 / dots;
+  for (let i = 0; i < dots; i++) {
+    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+  }
+
+  elem.innerHTML = points;
+  const pointsMarked = elem.querySelectorAll(".points");
+  for (let i = 0; i < percent; i++) {
+    pointsMarked[i].classList.add("marked");
+  }
+});
+
+// portbtn///////////////////////////////////////
+
+var buttons = document.querySelectorAll(".portbtn");
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    buttons.forEach(function (btn) {
+      btn.classList.remove("portactive");
+    });
+    button.classList.add("portactive");
+  });
+});
